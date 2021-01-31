5c4af1305d005061f749d78145c5f1e900fa2fcf
Xartiou <xartiou@gmail.com>
2021-01-31 20:44:37 +0300

    clear repo

--- home_work/Less_1/task_2.py
+++
@@ -1,8 +0,0 @@
-# Пользователь вводит время в секундах.
-# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
-# Используйте форматирование строк.
-quantity_sec = int(input("Введите время в секундах "))
-hours = quantity_sec // 3600
-minutes = (quantity_sec - hours * 3600) // 60
-seconds = quantity_sec - (hours * 3600 + minutes * 60)
-print(f'Секунды в формате чч:мм:сс   {hours} : {minutes} : {seconds}')
